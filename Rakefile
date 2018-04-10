5eed807c59ce0c60a8ba580a85bd13a9e3b14361
Paul Padier <paul.padier@rainforestapp.com>
2018-04-10 14:05:03 +0900

    Don't hit Sauce by default in specs

--- Rakefile
+++ Rakefile
@@ -2,6 +2,7 @@ require 'bundler/gem_tasks'
 
 begin
   require 'rspec/core/rake_task'
+  ENV['CAPYBARA_DRIVER'] = 'selenium'
   RSpec::Core::RakeTask.new(:spec) do |t|
     t.rspec_opts = '--color --format documentation'
   end
---
+++ spec/rainforest_test_spec.rb
@@ -0,0 +1,75 @@
+require 'open3'
+require_relative './spec_helper'
+
+module RainforestRubyRuntime
+  describe 'rainforest_test' do
+    subject do
+      if browsers
+        Open3.capture2e("ruby bin/rainforest_test #{browsers} sample_tests/empty.rb")
+      else
+        Open3.capture2e('ruby bin/rainforest_test sample_tests/empty.rb')
+      end
+    end
+
+    context 'with no browsers' do
+      let(:browsers) { nil }
+
+      it 'defaults to chrome' do
+        output, status = subject
+
+        expect(status).to be_success
+        expect(output).to match(/chrome/)
+      end
+    end
+
+    context 'with a browser' do
+      context 'from the whitelist' do
+        let(:browser) { Runner::BROWSERS.sample }
+        let(:browsers) { "--browsers #{browser}" }
+
+        it 'passes it to Runner' do
+          output, status = subject
+
+          expect(status).to be_success
+          expect(output).to match(browser)
+        end
+      end
+
+      context 'that is unknown' do
+        let(:browsers) { "--browsers opera" }
+
+        it 'exits with status 1' do
+          output, status = subject
+
+          expect(status).to_not be_success
+          expect(output).to match(/invalid option: #{browsers}/)
+        end
+      end
+    end
+
+    context 'with multiple browsers' do
+      context 'from the whitelist' do
+        let(:browser_values) { Runner::BROWSERS.sample(2) }
+        let(:browsers) { "--browsers #{browser_values.join(',')}" }
+
+        it 'passes them to Runner' do
+          output, status = subject
+
+          expect(status).to be_success
+          browser_values.each { |b| expect(output).to match(b) }
+        end
+      end
+
+      context 'that is unknown' do
+        let(:browsers) { "--browsers #{Runner::BROWSERS.sample},opera" }
+
+        it 'exits with status 1' do
+          output, status = subject
+
+          expect(status).to_not be_success
+          expect(output).to match(/invalid option: --browsers opera/)
+        end
+      end
+    end
+  end
+end
